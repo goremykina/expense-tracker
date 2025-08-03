2e38b734800795b852e016fb65d8e948a3aa5229
Alina Goremykina <a.goremykina@godeltech.com>
2025-08-03 21:59:48 +0200

    Add ping endpoint

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello World!");
+import express from 'express';
+import { Request, Response } from 'express';
+
+const app = express()
+const port = 3001
+
+app.get('/ping', (req: Request, res: Response) => {
+  res.send({ message: 'pong' })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
