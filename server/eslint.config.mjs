4f6770b885f265ac7927a8acfe4662bc2d02da01
Alina Goremykina <a.goremykina@godeltech.com>
2025-08-04 22:13:47 +0200

    Add linter and prettier

---
+++ server/eslint.config.mjs
@@ -0,0 +1,21 @@
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import prettierPlugin from 'eslint-plugin-prettier'
+import prettierConfig from 'eslint-config-prettier'
+
+export default tseslint.config(
+    eslint.configs.recommended,
+    tseslint.configs.recommended,
+    {
+        plugins: {
+            prettier: prettierPlugin,
+        },
+        rules: {
+            'prettier/prettier': 'error',
+            ...prettierConfig.rules,
+        },
+    },
+    {
+        ignores: ['node_modules', 'dist'],
+    },
+)
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,13 +1,13 @@
-import express from 'express';
-import { Request, Response } from 'express';
+import express from 'express'
+import { Request, Response } from 'express'
 
 const app = express()
 const port = 3001
 
 app.get('/ping', (req: Request, res: Response) => {
-  res.send({ message: 'pong' })
+    res.send({ message: 'pong' })
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
