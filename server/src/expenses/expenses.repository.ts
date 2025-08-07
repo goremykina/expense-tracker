3c774cf403aff5c37d56af2e07268d20a3b4593d
Alina Goremykina <a.goremykina@godeltech.com>
2025-08-07 23:04:26 +0200

    Use entity

--- server/src/expenses/expenses.repository.ts
+++ server/src/expenses/expenses.repository.ts
@@ -1,6 +1,7 @@
-import { Expense, PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client'
 import { CreateExpenseDto } from './dto/create-expense.dto'
 import { UpdateExpenseDto } from './dto/update-expense.dto'
+import { Expense } from './entity/expense.entity'
 
 const prisma = new PrismaClient()
 
@@ -16,7 +17,7 @@ export const ExpensesRepository = {
         fromDate: string
         toDate: string
     }): Promise<Expense[]> => {
-        return await prisma.expense.findMany({
+        const expenses = await prisma.expense.findMany({
             where: {
                 AND: [fromDate ? { date: { gte: fromDate } } : {}, toDate ? { date: { lte: toDate } } : {}],
             },
@@ -26,27 +27,48 @@ export const ExpensesRepository = {
                 date: 'desc',
             },
         })
+        return expenses.map((expense) => ({
+            ...expense,
+            amount: expense.amount.toNumber(),
+        }))
     },
 
     createExpense: async (data: CreateExpenseDto): Promise<Expense> => {
-        return await prisma.expense.create({ data })
+        const expense = await prisma.expense.create({ data })
+        return {
+            ...expense,
+            amount: expense.amount.toNumber(),
+        }
     },
 
     getExpenseById: async (id: number): Promise<Expense | null> => {
-        return await prisma.expense.findUnique({
+        const expense = await prisma.expense.findUnique({
             where: { id },
         })
+
+        if (!expense) {
+            return null
+        }
+
+        return {
+            ...expense,
+            amount: expense.amount.toNumber(),
+        }
     },
 
-    updateExpense: async (id: number, data: UpdateExpenseDto) => {
-        return await prisma.expense.update({
+    updateExpense: async (id: number, data: UpdateExpenseDto): Promise<Expense> => {
+        const expense = await prisma.expense.update({
             where: { id },
             data,
         })
+        return {
+            ...expense,
+            amount: expense.amount.toNumber(),
+        }
     },
 
     deleteExpense: async (id: number) => {
-        return await prisma.expense.delete({
+        await prisma.expense.delete({
             where: { id },
         })
     },
