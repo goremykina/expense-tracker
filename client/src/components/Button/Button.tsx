07a54da6e224f6381c067518ed782ac77fe29891
Alina Goremykina <a.goremykina@godeltech.com>
2025-08-10 23:44:15 +0200

    Add Button

---
+++ client/src/components/Button/Button.tsx
@@ -0,0 +1,26 @@
+import  styles from './Button.module.css'
+
+interface ButtonProps {
+    text: string,
+    disabled?: boolean,
+    variant: 'primary' | 'ghost'
+}
+
+export function Button({ text, disabled, variant }: ButtonProps) {
+    const getVariantClassName = (): string =>  {
+        switch (variant) {
+            case 'primary':
+                return 'primary'
+            case 'ghost':
+                return 'ghost'
+            default:
+                return ''
+        }
+    }
+
+    return (
+        <button className={`${styles.button} ${styles[getVariantClassName()]}  ${disabled ? styles.disabled : ''}`} disabled={disabled}>
+            {text}
+        </button>
+    )
+}
