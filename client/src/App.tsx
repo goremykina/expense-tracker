0b27ad5100a5d88fe5f53dd372a871cac62d317c
Alina Goremykina <a.goremykina@godeltech.com>
2025-08-12 15:10:58 +0200

    Add DatePicker

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,29 @@
 import './App.css'
-import { Header } from "./components/Header";
-import { Loader } from "./components/Loader";
+import { DatePicker } from './components/DatePicker'
+import { Header } from './components/Header'
+import { Icon } from './components/Icon'
+import { Input } from './components/Input'
+import { InputLabel } from './components/InputLabel'
+import { Loader } from './components/Loader'
 
 function App() {
-
     return (
         <div>
             <Header />
+            <Icon iconName="bell" size={15} />
             <Loader />
+            <DatePicker />
+
+            <>
+                <InputLabel htmlFor='user'/>
+                <Input
+                    placeholder='Text input'
+                    helperText='Caption'
+                    id={'user'}
+                />
+            </>
         </div>
     )
 }
 
-export default App;
+export default App
---
+++ client/src/components/DatePicker/DatePicker.tsx
@@ -0,0 +1,17 @@
+import styles from './DatePicker.module.css'
+import { useState } from 'react'
+
+
+export const DatePicker = () => {
+    const [selectedDate, setSelectedDate] = useState<string>('2023-10-01')
+    console.log(selectedDate)
+
+    return (
+        <input
+            type="date"
+            value={selectedDate}
+            className={styles['datePicker-input']}
+            onChange={(e) => setSelectedDate(e.target.value)}
+        />
+    )
+}
